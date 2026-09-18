49b0f981220c1b2f6bf83f633284dc20d65b5751
Jean Luc Kawel <20ka208@esisalama.org>
2026-09-18 17:21:35 +0200

    skip: update RolePermissionSeeder to comment out purchase order permissions and enable procurement permissions

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -230,16 +230,16 @@ class RolePermissionSeeder extends Seeder
 
 
             // Purchase Orders
-            'purchase_orders.view',
-            'purchase_orders.create',
-            'purchase_orders.delete',
+//            'purchase_orders.view',
+//            'purchase_orders.create',
+//            'purchase_orders.delete',
 
 
 
             // Procurement
-//            'procurement.view',
-//            'procurement.approve',
-//            'procurement.reject',
+            'procurement.view',
+            'procurement.approve',
+            'procurement.reject',
 
 
         ]);
